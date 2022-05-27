ecb089725205176a98abe7d4cd7a421117a0c801
Angie Cabanillas <acabanillasb2@upao.edu.pe>
2022-05-27 15:43:55 -0500

    chore: implementing delete event

--- frontend_ecoevent/src/app/Components/evento/evento.component.ts
+++ frontend_ecoevent/src/app/Components/evento/evento.component.ts
@@ -1,37 +1,50 @@
 import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { EventoService } from 'src/app/services/evento.service';
 export interface Eventos {
-  id:string;
-  titulo:string;
-  descripcion:string;
-  lugar:string;
-  hora:string;
-  fecha:string;
-  foto:string;
+  id: string;
+  titulo: string;
+  descripcion: string;
+  lugar: string;
+  hora: string;
+  fecha: string;
+  foto: string;
 }
 
-const ELEMENT_DATA: Eventos[] = [
-  {id: '1', titulo: 'Recojo de basura en las playas', descripcion: '', lugar: 'Trujillo', hora:'21',fecha:'01/02/2001',foto:'url'},
-  {id: '2', titulo: 'Campaña de reciclaje', descripcion: '', lugar: 'Laredo', hora:'',fecha:'01/02/2001',foto:'url'},
-  {id: '3', titulo: 'Recojo de basura en las playas', descripcion: '', lugar: 'El porvenir', hora:'21',fecha:'01/02/2001',foto:'url'},
-  {id: '4', titulo: 'Campaña de reciclaje', descripcion: '', lugar: 'Trujillo', hora:'21',fecha:'01/02/2001',foto:'url'},
-  {id: '5', titulo: 'Recojo de basura en las playas', descripcion: '', lugar: 'La esperanza', hora:'21',fecha:'01/02/2001',foto:'url'},
-  {id: '6', titulo: 'Campaña de reciclaje', descripcion: '', lugar: 'Trujillo', hora:'21',fecha:'01/02/2001',foto:'url'},
-  {id: '7', titulo: 'Recojo de basura en las playas', descripcion: '', lugar: 'N', hora:'21',fecha:'01/02/2001',foto:'url'},
-  {id: '8', titulo: 'Campaña de reciclaje', descripcion: '', lugar: 'Huanchaco', hora:'21',fecha:'01/02/2001',foto:'url'},
-  {id: '9', titulo: 'Recojo de basura en las playas', descripcion: '', lugar: 'Florencia de Mora', hora:'21',fecha:'01/02/2001',foto:'url'},
-  {id: '10', titulo: 'Campaña de reciclaje', descripcion: '', lugar: 'Trujillo', hora:'21',fecha:'01/02/2001',foto:'url'},
-];
 @Component({
   selector: 'app-evento',
   templateUrl: './evento.component.html',
-  styleUrls: ['./evento.component.css']
+  styleUrls: ['./evento.component.css'],
 })
 export class EventoComponent implements OnInit {
+  listEventos: Eventos[] = [];
 
-  constructor() { }
+  displayedColumns: string[] = [
+    'id',
+    'titulo',
+    'descripcion',
+    'lugar',
+    'hora',
+    'fecha',
+    'foto',
+    'botones',
+  ];
+  dataSource!: MatTableDataSource<any>;
+
+  constructor(private _eventoService: EventoService) {}
 
   ngOnInit(): void {
+    this.cargarEventos();
   }
-  displayedColumns: string[] = ['id','titulo','descripcion','lugar','hora','fecha','foto','botones'];
-  dataSource = ELEMENT_DATA;
+
+  cargarEventos() {
+    this.listEventos = this._eventoService.getEventos();
+    this.dataSource = new MatTableDataSource(this.listEventos);
+  }
+
+  eliminarEvento(index: number) {
+    console.log(index);
+    this._eventoService.eliminarEvento(index);
+    this.cargarEventos();
+  }
 }
