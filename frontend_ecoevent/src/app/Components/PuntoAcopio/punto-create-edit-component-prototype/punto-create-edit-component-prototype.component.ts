9438488b68651453ceee4a9852013ee75ba3f93b
Lourdes Cornejo Adrianzen <mcornejoa1@upao.edu.pe>
2022-07-13 23:04:00 -0500

    chore: editar Punto Acopio

--- frontend_ecoevent/src/app/Components/PuntoAcopio/punto-create-edit-component-prototype/punto-create-edit-component-prototype.component.ts
+++ frontend_ecoevent/src/app/Components/PuntoAcopio/punto-create-edit-component-prototype/punto-create-edit-component-prototype.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { PuntoService } from 'src/app/services/punto.service';
 import { PuntoAcopio } from '../Prototype/PuntoAcopio';
+
 @Component({
   selector: 'app-punto-create-edit-component-prototype',
   templateUrl: './punto-create-edit-component-prototype.component.html',
@@ -10,9 +11,36 @@ import { PuntoAcopio } from '../Prototype/PuntoAcopio';
 export class PuntoCreateEditComponentPrototypeComponent implements OnInit {
   acto = 'Crear';
   
-  constructor() { }
+  constructor(
+    private _puntoService: PuntoService,
+    private rutaPuntos: ActivatedRoute,
+    private _router: Router
+  ) { }
+
+  index: number = this.rutaPuntos.snapshot.params['id'];
+
+  puntos: PuntoAcopio[] = [];
 
   ngOnInit(): void {
   }
 
+  //para identificar 'editarPuntos'
+  accion: boolean = this.puntos[this.index - 1] ? false : true;
+
+  esEditar() {
+    if (this.index === 0) {
+      this.acto = 'Agregar';
+    }
+  }
+
+  puntosMostrar: PuntoAcopio = {
+    direccion: '',
+    horario: '',
+    distrito: '',
+  };
+
+  editarPunto() {
+    this._puntoService.editarPunto(this.puntosMostrar).subscribe();
+    this._router.navigate(['/puntoAcopio']);
+  }
 }
--- frontend_ecoevent/src/app/services/punto.service.ts
+++ frontend_ecoevent/src/app/services/punto.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
+import { environment } from 'src/environments/environment';
+import { PuntoAcopio } from '../Components/PuntoAcopio/Prototype/PuntoAcopio';
 @Injectable({
   providedIn: 'root'
 })
 export class PuntoService {
+  private apiUrl = `${environment.API}/puntos`;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getPuntos() {
+    return this.http.get<PuntoAcopio[]>(`${this.apiUrl}`);
+  }
+
+  editarPunto(dto: PuntoAcopio) {
+    return this.http.put<PuntoAcopio>(`${this.apiUrl}`, dto);
+  }
 }
