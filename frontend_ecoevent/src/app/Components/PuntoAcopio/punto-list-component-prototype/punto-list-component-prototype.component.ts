f02254c56d9f09a1505fc95ca1b603b3142c6f07
Kiara Requenes Querevalu <krequenesq1@upao.edu.pe>
2022-07-14 23:50:15 -0500

    chore: add method create

--- frontend_ecoevent/src/app/Components/PuntoAcopio/punto-list-component-prototype/punto-list-component-prototype.component.ts
+++ frontend_ecoevent/src/app/Components/PuntoAcopio/punto-list-component-prototype/punto-list-component-prototype.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
-
+import { PuntoService } from 'src/app/services/punto.service';
+import { PuntoAcopio } from '../Prototype/PuntoAcopio';
 @Component({
   selector: 'app-punto-list-component-prototype',
   templateUrl: './punto-list-component-prototype.component.html',
   styleUrls: ['./punto-list-component-prototype.component.css']
 })
 export class PuntoListComponentPrototypeComponent implements OnInit {
+  listPuntos: PuntoAcopio[] = [];
 
   displayedColumns: string[] = [
     'id',
@@ -17,10 +19,18 @@ export class PuntoListComponentPrototypeComponent implements OnInit {
   ];
   dataSource!: MatTableDataSource<any>;
 
-  constructor() {}
+  constructor(private _puntoService: PuntoService) {}
 
   ngOnInit(): void {
-    
+    this.cargarPuntos();
+  }
+  crearPunto(punto: PuntoAcopio) {
+    this._puntoService.crearPunto(punto);
+  }
+  cargarPuntos() {
+    this._puntoService.getPuntos().subscribe((data) => {
+      this.dataSource = new MatTableDataSource(data);
+    });
   }
 
 
